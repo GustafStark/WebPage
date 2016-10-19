2e6b2cdce45f68d85b4e4006a6a985d949874888
GustafStark <gustafstark@gmail.com>
2016-10-19 09:38:51 +0200

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HRMVer4.Startup))]
+namespace HRMVer4
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
